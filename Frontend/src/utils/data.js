89896122da60673fd1a8249eb3e44d79396b9526
sagar vashist <sagarheromail10@gmail.com>
2026-04-06 12:29:27 +0530

    Initial commit22

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,6 +1,6 @@
-const BASE_URL = "https://job-portal-4o8h.onrender.com"; 
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const USER_API_ENDPOINT = `${BASE_URL}/api/users`;
-export const JOB_API_ENDPOINT = `${BASE_URL}/api/job`;
-export const APPLICATION_API_ENDPOINT = `${BASE_URL}/api/application`;
-export const COMPANY_API_ENDPOINT = `${BASE_URL}/api/company`;
+export const USER_API_ENDPOINT = `${BASE_URL}${import.meta.env.VITE_USER_API_ENDPOINT}`;
+export const JOB_API_ENDPOINT = `${BASE_URL}${import.meta.env.VITE_JOB_API_ENDPOINT}`;
+export const APPLICATION_API_ENDPOINT = `${BASE_URL}${import.meta.env.VITE_APPLICATION_API_ENDPOINT}`;
+export const COMPANY_API_ENDPOINT = `${BASE_URL}${import.meta.env.VITE_COMPANY_API_ENDPOINT}`;
